6399513af9f15f8c26e696467042bcd97dd2dfb5
QueenColly <colleteumeh0@gmail.com>
2026-07-24 11:24:51 +0100

    batch commit

---
+++ src/ng/ourChemo/data/models/Batch.java
@@ -0,0 +1,16 @@
+package ng.ourChemo.data.models;
+
+import java.time.LocalDate;
+
+public class Batch {
+    private int quantityLeft;
+    private int costPrice;
+    private int purchaseQuantity;
+    private LocalDate purchasedDate;
+    private int id;
+    private LocalDate expiryYearMonth;
+    private int drugId;
+
+
+
+}
