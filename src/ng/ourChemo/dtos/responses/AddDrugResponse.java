90e6e5c25a307caaa8bfb8699705612411a7870b
QueenColly <colleteumeh0@gmail.com>
2026-07-24 11:23:48 +0100

    add drug response commit

---
+++ src/ng/ourChemo/dtos/responses/AddDrugResponse.java
@@ -0,0 +1,9 @@
+package ng.ourChemo.dtos.responses;
+
+public class AddDrugResponse {
+    private String brand;
+    private int totalDrugsAvailable;
+    private String drugName;
+    private int totalDrugAdded;
+    private int batchId;
+}
